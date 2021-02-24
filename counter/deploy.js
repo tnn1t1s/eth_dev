90d0d2a4eff72a679f121d439d075ca1d1256510
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-24 08:39:50 -0500

    this works but program does not terminate. it *does* create a new contract on the network visible on etherscan.io

--- counter/deploy.js
+++ counter/deploy.js
@@ -5,23 +5,28 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile.js');
 
-const provider = new HDWallerProvider(
-     'alert baby immune ride daughter clerk loyal group ready oppose tooth increase',
-     'https://mainnet.infura.io/v3/c68405b604f443f6b64cdd363a0282cf'
+const ropsten_endpoint = 'https://ropsten.infura.io/v3/c68405b604f443f6b64cdd363a0282cf'
+const mnemonic = 'alert baby immune ride daughter clerk loyal group ready oppose tooth increase'
+
+const provider = new HDWalletProvider(
+     mnemonic,
+     ropsten_endpoint
 );
 
 const web3 = new Web3(provider);
+
+console.log(web3.eth);
 
 
 // async/await can only exist in a function
 const deploy = async () => {
      const accounts = await web3.eth.getAccounts();
-     console.log('Attempting to deploy from account',
-                 accounts[0]);
-     const result = await new web3.eth.Contract(JSON.parse(interface))
-     .deploy({data: '0x' + bytecode, arguments: [1]}) 
-     .send({from: accounts[0]});
+     console.log('Attempting to deploy from account',accounts[0]);
+     const result = await new web3.eth.Contract(interface).deploy({data: '0x' + bytecode, arguments: [1]}).send({from: accounts[0]});
+     console.log(result.options.address);
+};
 
-deploy() 
-
-
+console.log('about to deploy ...');
+deploy().then( () => {
+     console.log('deployed');
+});
