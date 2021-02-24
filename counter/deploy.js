72310cab139e6433a9d592cdec93b552903e3c95
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-23 20:48:51 -0500

    testing on Rotsten network

--- counter/deploy.js
+++ counter/deploy.js
@@ -1,4 +1,5 @@
-// script to deploy Counter to Rinkeby network through Infura
+// script to deploy Counter to a test network through Infura
+// For this test, I'm using Ropsten network
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
@@ -10,5 +11,17 @@ const provider = new HDWallerProvider(
 );
 
 const web3 = new Web3(provider);
+
+
+// async/await can only exist in a function
+const deploy = async () => {
+     const accounts = await web3.eth.getAccounts();
+     console.log('Attempting to deploy from account',
+                 accounts[0]);
+     const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data: '0x' + bytecode, arguments: [1]}) 
+     .send({from: accounts[0]});
+
+deploy() 
 
 
