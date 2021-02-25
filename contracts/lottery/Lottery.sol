68cee4ff10b725f72012809d4173e2ffedd03310
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-25 18:16:18 -0500

    require $$$

--- contracts/lottery/Lottery.sol
+++ contracts/lottery/Lottery.sol
@@ -3,8 +3,17 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract Lottery {
      address public manager;
+     address[] public players;
      
      constructor() public {
           manager = msg.sender;
+     }
+
+     function enter() public payable {
+          // set a minimun amount of ether 
+          // required to run this function!!!
+          require(msg.value > .001 ether);
+          // if paid, do work ...
+          players.push(msg.sender); 
      }
 }
