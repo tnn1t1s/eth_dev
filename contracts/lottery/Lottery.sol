b694b4482f0f3fe1965c141ff7e024f5da79dc46
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-26 19:52:42 -0500

    added RNG

--- contracts/lottery/Lottery.sol
+++ contracts/lottery/Lottery.sol
@@ -1,19 +1,22 @@
-// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Lottery {
      address public manager;
      address[] public players;
-     
-     constructor() public {
+    
+     constructor() {
           manager = msg.sender;
      }
 
      function enter() public payable {
-          // set a minimun amount of ether 
-          // required to run this function!!!
-          require(msg.value > .001 ether);
-          // if paid, do work ...
-          players.push(msg.sender); 
+         require(msg.value > 0.1 ether);
+         players.push(msg.sender); 
      }
+     
+     function random() private view returns (uint256) {
+         return uint256(keccak256(abi.encodePacked(block.difficulty,
+                                                   block.timestamp,
+                                                   players)));
+     } 
 }
+
