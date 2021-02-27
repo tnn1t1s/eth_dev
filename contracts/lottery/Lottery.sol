98e1fb443c20b4256a488144a5cf44d0b6993cec
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-26 21:27:18 -0500

    use modifiers and understand memory v. storage vars

--- contracts/lottery/Lottery.sol
+++ contracts/lottery/Lottery.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Lottery {
      address public manager;
-     address[] public players;
+     address payable[] public players;
     
      constructor() {
           manager = msg.sender;
@@ -14,9 +15,31 @@ contract Lottery {
      }
      
      function random() private view returns (uint256) {
-         return uint256(keccak256(abi.encodePacked(block.difficulty,
-                                                   block.timestamp,
-                                                   players)));
+         return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
      } 
+     
+     function payWinner() public restricted {
+         uint index = random() % players.length;
+         address contractAddress = address(this);
+         players[index].transfer(contractAddress.balance);
+         resetContract();
+     }
+     
+     
+     function resetContract() private {
+         players = new address payable[](0);
+     }
+     
+     function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+     
+     modifier restricted() {
+         require(msg.sender == manager);
+         _;
+     }
+     
+     
 }
+
 
