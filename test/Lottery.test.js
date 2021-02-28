db08681228061444860b051b8936ebbe9161d10c
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-28 10:31:14 -0500

    tests for Lottery contract and slightly improved error handling in utils.compile

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,5 +1,30 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
 const web3 = new Web3(ganache.provider());
 const utils = require('../utils.js');
+
+let interface, bytecode;
+let lottery;
+let accounts;
+
+beforeEach(async() => {
+    contract = utils.compile('./contracts/lottery/Lottery.sol');
+    interface = contract.abi;
+    bytecode = contract.evm.bytecode.object;
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(contract.abi)
+      .deploy({ data: bytecode })
+      .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+     it('deploys', () => {
+          assert.ok(lottery.options.address);
+     });
+});
+
+
+
--- utils.js
+++ utils.js
@@ -7,6 +7,7 @@ const solc = require('solc');
  * Returns a `contract` where
  * the contract "interface" is contract.abi and
  * the compiled "bytecode" is contract.evm.bytecode.object
+ * compiler doc: https://docs.soliditylang.org/en/v0.4.24/using-the-compiler.html
  *
  * @param {string} contractUri The path to the contract source
  * @return {contract} a solc compiled contract object
@@ -30,6 +31,8 @@ const compile = (contractUri) => {
     };
     input['sources'][contractFilename] = {content : contractSource };
     output = JSON.parse(solc.compile(JSON.stringify(input)))
+    if(output['errors'])
+         return output;
     contracts = output.contracts[contractFilename];
     contract = contracts[contractName];
     return contract;
