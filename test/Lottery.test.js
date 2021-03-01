1d256bf8a0fdcfbfb8ba14569aa810bdd655082f
tnn1t1s <david.j.palaitis@gmail.com>
2021-02-28 19:15:25 -0500

    added an end to end test that plays full game

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -81,6 +81,31 @@ describe('Lottery Contract', () => {
               assert(err);
           }
      });
+
+     it('only manager can call pick winner', async () => {
+          try {
+               await lottery.methods.payWinner.send({
+                    from: accounts[1]
+               });
+               assert(false);
+          } catch (err) {
+              assert(err);
+          }
+     });
+
+     it('executes end to end contract and resets', async () => {
+         await lottery.methods.enter().send({
+              from: accounts[0],
+              value: web3.utils.toWei('2', 'ether')
+         });
+
+         const initialBalance = await web3.eth.getBalance(accounts[0]);
+         await lottery.methods.payWinner().send({ from: accounts[0] });
+         const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+         const difference = (finalBalance - initialBalance);
+         assert(difference > web3.utils.toWei('1.8'), 'ether');
+     });
 });
 
 
