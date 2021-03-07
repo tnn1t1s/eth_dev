24ac70bef0d49e26dca461f28e67f58892bc36bd
tnn1t1s <david.j.palaitis@gmail.com>
2021-03-07 13:43:37 -0500

    updated to use dicts for constant time lookup

--- contracts/coinstarter/ballot.sol
+++ contracts/coinstarter/ballot.sol
@@ -7,12 +7,14 @@ contract Campaign {
          uint value;
          address recipient;
          bool complete;
+         uint approvalCount;
+         mapping(address => bool) approvals;
      }
 
      address public manager;
-     address[] public approvers;
      Request[] public requests;
      uint public minContribution;
+     mapping(address => bool) public approvers;
 
      modifier restricted() {
          require(msg.sender == manager);
@@ -24,9 +26,21 @@ contract Campaign {
          minContribution = minimum;
      }
 
+     function createRequest(string description,
+                            uint value,
+                            address recipient) public restricted {
+          Request request = Request({
+                  description: description,
+                  value: value,
+                  recipient: recipient,
+                  complete: false});
+
+          requests.push(request);
+     }
+
      function contribute() public payable {
-       require(msg.value >= minContribution);
-       approvers.push(msg.sender);
+        require(msg.value >= minContribution);
+        approvers[msg.sender] = true;
      }
 }
 
