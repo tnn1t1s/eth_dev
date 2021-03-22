4becc3a6c39a83d860ad944117a28c2fbd45b33d
tnn1t1s <david.j.palaitis@gmail.com>
2021-03-22 14:06:06 -0400

    this works w/ Web3 but still cant figure out ethers deployment to Infura. moving on. will circle back to this

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -6,15 +6,23 @@ const Web3 = require('web3');
 const ropsten_endpoint = 'https://ropsten.infura.io/v3/c68405b604f443f6b64cdd363a0282cf'
 const mnemonic = 'alert baby immune ride daughter clerk loyal group ready oppose tooth increase'
 
+c = require('./build/CampaignFactory.json');
+
 const provider = new HDWalletProvider(
      mnemonic,
      ropsten_endpoint
 );
 const web3 = new Web3(provider);
 
-// load from files
-const interface;
-const bytecode;
+/*
+ * the contract "interface" is contract.abi and
+ * the compiled "bytecode" is contract.evm.bytecode.object
+ */
+
+console.log(c.abi);
+console.log(c.evm.bytecode.object);
+const interface=c.abi;
+const bytecode=c.evm.bytecode.object;
 
 const deploy = async () => {
      const accounts = await web3.eth.getAccounts();
