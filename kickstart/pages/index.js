fc17a2b8241628c23a9a095a1577096255227b30
tnn1t1s <david.j.palaitis@gmail.com>
2021-03-23 09:34:36 -0400

    still working around next/truffle issues but this is now end to end on browser side

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {signer, provider} from '../ethereum/ethers';
+import contract from '../ethereum/factory';
 
-export default () => {
-  return <h1>show page</h1>
-};
+class CampaignIndex extends Component {
+    state = {
+        campaigns: []
+    }
+    /* getInitialProps is specific to Next framework
+     * it allows for upfront data loading on server
+     * it is `static` to allow for data fetching
+     * without instantiation of the Component
+    static async getInitialProps() {
+        const campaigns = await contract.getDeployedCampaigns();
+        return { campaigns };
+    }
+    */
+
+    async componentDidMount() {
+        await window.ethereum.enable();
+        const campaigns = await contract.getDeployedCampaigns();
+        this.setState({campaigns});
+    }
+
+    render() {
+        return <div>{this.state.campaigns[0]}</div>
+    }
+}
+
+export default CampaignIndex;
