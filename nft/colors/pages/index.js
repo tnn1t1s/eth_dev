8a1b6126a472d545357a4862845eddeecfe6a5d7
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-19 20:56:45 -0400

    Issue #5 - bootstrap NFT/colors

---
+++ nft/colors/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React, { Component } from 'react';
+
+
+class Colors extends Component {
+  render() {
+    return (<div><h1>Colors</h1></div>);
+  }
+}
+
+export default Colors;
