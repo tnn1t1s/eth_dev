72e788bdf216bf7a662a1b57ea53137abd194400
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-20 11:10:57 -0400

    react/next bootstrap plus first draft contract

---
+++ nft/colors/ethereum/contracts/Color.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Color is ERC721 {
+  string[] public colors;
+  mapping(string => bool) _colorExists;
+
+  constructor() ERC721("Color", "COLOR") public {
+  }
+
+  // E.G. color = "#FFFFFF"
+  function mint(string memory _color) public {
+    require(!_colorExists[_color]);
+    uint _id = colors.push(_color);
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+  }
+
+}
--- nft/colors/pages/index.js
+++ nft/colors/pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import React, { Component } from 'react';
+import Navbar from '../components/Navbar'
+import Layout from '../components/Layout';
 
 
 class Colors extends Component {
   render() {
-    return (<div><h1>Colors</h1></div>);
+    return (
+        <Layout>
+          <div>Colors</div>
+        </Layout>
+    );
   }
 }
 
