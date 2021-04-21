add0c6c38beff5f129fc80e80cdbfce136a09adb
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-20 22:07:33 -0400

    no stackoverflow here, need to actually read the docs https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol

--- nft/colors/test/Color.test.js
+++ nft/colors/test/Color.test.js
@@ -7,12 +7,37 @@ require('chai')
 
 contract('Color', (accounts) => {
   let contract;
-  
+
+  before(async () => {
+    contract = await Color.deployed();
+  });
+
   describe('deployment', async() => {
+
     it('deploys', async () => {
-      contract = await Color.deployed();
       const address = contract.address;
       assert.notEqual(address, '');
+      assert.notEqual(address, null);
     });
+
+    it('has a name', async () => {
+      const name = await contract.name();
+      assert.equal(name, 'Color');
+    });
+
+    it('has a symbol', async () => {
+      const symbol = await contract.symbol();
+      assert.equal(symbol, 'COLOR');
+    });
+  });
+
+  describe('deployment', async() => {
+
+    it('creates a new token', async () => {
+      const result = await contract.mint("#EC058E");
+      const totalSupply = await contract.totalSupply();
+      assert.equal(totalSupply, 1);
+    });
+
   });
 });
