e066473564eb0674dede93ba0a50c3a2bbcb7995
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-26 21:45:53 -0400

    added chainlink callbacks for random number generation

--- nft/fractal/contracts/Fractal.sol
+++ nft/fractal/contracts/Fractal.sol
@@ -1,25 +1,58 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.0;
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Fractal is ERC721 {
-  uint256[] public fractals;
-  mapping(uint256 => bool) _fractalExists;
+contract Fractal is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    uint256 randomVariable;
+    // add other things
+    mapping(bytes32 => address) public requestIdToSender;
+    mapping(bytes32 => string) public requestIdToTokenURI;
+    mapping(uint256 => uint256) public tokenIdToRandoVariable;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+    event requestedCollectible(bytes32 indexed requestId);
 
-  constructor() ERC721("Fractal", "FRACTAL") public {
-  }
 
-  function mint() public {
-    // TODO: get this fractal from chainlink randomness
-    uint _fractal = 0;
-    require(!_fractalExists[_fractal]);
-    // push this fractal to the fractals array
-    fractals.push(_fractal);
-    // send this token to the msg.sender address
-    _mint(msg.sender, fractals.length - 1);
-    // bookkeep on this 'fractal'
-    _fractalExists[_fractal] = true;
-  }
+    bytes32 internal keyHash;
+    uint256 internal fee;
+
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
+    public
+    VRFConsumerBase(_VRFCoordinator, _LinkToken)
+    ERC721("Fractal", "FRACTAL")
+    {
+        tokenCounter = 0;
+        keyHash = _keyhash;
+        fee = 0.1 * 10 ** 18;
+    }
+
+    function createCollectible(string memory tokenURI, uint256 userProvidedSeed)
+        public returns (bytes32){
+            bytes32 requestId = requestRandomness(keyHash, fee, userProvidedSeed);
+            requestIdToSender[requestId] = msg.sender;
+            requestIdToTokenURI[requestId] = tokenURI;
+            emit requestedCollectible(requestId);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+        address owner = requestIdToSender[requestId];
+        string memory tokenURI = requestIdToTokenURI[requestId];
+        uint256 newItemId = tokenCounter;
+        _safeMint(owner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        randomVariable = randomNumber % 100;
+        tokenIdToRandoVariable[newItemId] = randomVariable;
+        requestIdToTokenId[requestId] = newItemId;
+        tokenCounter = tokenCounter + 1;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        _setTokenURI(tokenId, _tokenURI);
+    }
 }
