6a6afdda65a5a7e7715cff393ce68c94d0726cce
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-27 20:16:13 -0400

    finally found syntax for parameterized truffle deployments ... and spent $156 fake USD deploying the NFT factory

--- nft/fractal/migrations/2_deploy_contracts.js
+++ nft/fractal/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const Fractal = artifacts.require("Fractal");
 
+const vrfCoordinator = '0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9';
+const linkToken = '0xa36085f69e2889c224210f603d836748e7dc0088';
+const keyHash = '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4';
+
 module.exports = function(deployer) {
-  deployer.deploy(Fractal);
+  deployer.deploy(Fractal, vrfCoordinator, linkToken, keyHash);
 };
+
