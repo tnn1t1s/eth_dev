0166e62db56700c6bc71dd5bd8c787b58ca8b41b
tnn1t1s <david.j.palaitis@gmail.com>
2021-04-27 23:51:32 -0400

    manually tested mint function on Kovan. needed to fund the contract with Link. did this manually w/ metamask. todo: automate that in test

--- nft/fractal/contracts/Fractal.sol
+++ nft/fractal/contracts/Fractal.sol
@@ -4,27 +4,31 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
+
 contract Fractal is ERC721, VRFConsumerBase {
+    address public vrfCoordinator;
+    bytes32 internal keyHash;
+
     uint256 public tokenCounter;
     uint256 randomVariable;
     // add other things
     mapping(bytes32 => address) public requestIdToSender;
     mapping(bytes32 => string) public requestIdToTokenURI;
-    mapping(uint256 => uint256) public tokenIdToRandoVariable;
+    mapping(uint256 => uint256) public tokenIdToRandomVariable;
     mapping(bytes32 => uint256) public requestIdToTokenId;
     event requestedCollectible(bytes32 indexed requestId);
 
-
-    bytes32 internal keyHash;
     uint256 internal fee;
 
-    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
-    public
-    VRFConsumerBase(_VRFCoordinator, _LinkToken)
+    constructor(address _VRFCoordinator,
+                address _linkToken,
+                bytes32 _keyHash) public
+    VRFConsumerBase(_VRFCoordinator, _linkToken)
     ERC721("Fractal", "FRACTAL")
     {
         tokenCounter = 0;
-        keyHash = _keyhash;
+        vrfCoordinator =_VRFCoordinator;
+        keyHash = _keyHash;
         fee = 0.1 * 10 ** 18;
     }
 
@@ -43,7 +47,7 @@ contract Fractal is ERC721, VRFConsumerBase {
         _safeMint(owner, newItemId);
         _setTokenURI(newItemId, tokenURI);
         randomVariable = randomNumber % 100;
-        tokenIdToRandoVariable[newItemId] = randomVariable;
+        tokenIdToRandomVariable[newItemId] = randomVariable;
         requestIdToTokenId[requestId] = newItemId;
         tokenCounter = tokenCounter + 1;
     }
